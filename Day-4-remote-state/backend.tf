53f38fe55e02ae998a8777cfee10b31243bc00e7
Nazz <naziya8142@gmail.com>
2026-02-04 19:53:35 +0530

    Day-4-remote-state

--- Day-4-remote-state/backend.tf
+++ Day-4-remote-state/backend.tf
@@ -3,6 +3,10 @@ terraform {
     bucket = "dfgbnyhtgfvbghy"
     key    = "terraform.tfstate"
     region = "us-east-1"
+    use_lockfile = true # Enables S3 native locking
+   # This line is no longer needed/deprecated
   }
-
 }
+  
+
+
