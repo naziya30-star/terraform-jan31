ce471dde309f8960825ba13df5a5333aaa8aa418
Nazz <naziya8142@gmail.com>
2026-02-07 20:57:06 +0530

    Day-8

---
+++ Day-8-modules/variables.tf
@@ -0,0 +1,9 @@
+variable "ami_id" {
+  description = "AMI ID for EC2"
+  type        = string
+}
+
+variable "type" {
+  description = "EC2 instance type"
+  type        = string
+}
---
+++ Day-8-pass-values-to-module/main.tf
@@ -0,0 +1,7 @@
+module "dev" {
+    source = "../Day-8-modules"
+    ami_id = "ami-0532be01f26a3de55" 
+    type = "t3.small"
+
+
+}
